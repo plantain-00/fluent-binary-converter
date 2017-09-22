5d25cf1940e4cfdc4e857644c852fce4ce7d52c9
york yao <yaoao12306@outlook.com>
2017-09-22 15:38:12 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'dist/browser/index.js',
-  dest: 'dist/fluent-binary-converter.min.js',
-  format: 'umd',
-  moduleName: 'FluentBinaryConverter',
-  plugins: [resolve(), uglify()]
+  input: 'dist/browser/index.js',
+  name: 'FluentBinaryConverter',
+  plugins: [resolve(), uglify()],
+  output: {
+    file: 'dist/fluent-binary-converter.min.js',
+    format: 'umd'
+  }
 }
