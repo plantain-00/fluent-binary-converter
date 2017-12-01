b610d8c4e28b06f8686844958d860f15603bb8c4
york yao <yaoao12306@outlook.com>
2017-12-01 10:20:49 +0800

    feat: improve release script

--- clean-release.config.js
+++ clean-release.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   exclude: [
   ],
-  postScript: 'npm publish [dir] --access public',
-  base: 'dist'
+  base: 'dist',
+  askVersion: true,
+  postScript: [
+    'npm publish [dir] --access public',
+    'git add package.json',
+    'git commit -m "feat: publish v[version]"',
+    'git tag v[version]',
+    'git push',
+    'git push origin v[version]'
+  ]
 }
