52a576f0d7c86b74594b38d7fe040559cb74ebc0
york yao <yaoao12306@outlook.com>
2018-05-01 17:31:12 +0800

    build: add tslint-sonarts

--- src/index.ts
+++ src/index.ts
@@ -3,60 +3,60 @@
  */
 export class BinaryDecoder {
   private dataView: DataView
-  constructor (public arrayBuffer: ArrayBuffer, public index = 0) {
+  constructor(public arrayBuffer: ArrayBuffer, public index = 0) {
     this.dataView = new DataView(arrayBuffer)
   }
 
-  public getInt8 () {
+  public getInt8() {
     const result = this.dataView.getInt8(this.index)
     this.index += 1
     return result
   }
-  public getUint8 () {
+  public getUint8() {
     const result = this.dataView.getUint8(this.index)
     this.index += 1
     return result
   }
 
-  public getInt16 (littleEndian = true) {
+  public getInt16(littleEndian = true) {
     const result = this.dataView.getInt16(this.index, littleEndian)
     this.index += 2
     return result
   }
-  public getUint16 (littleEndian = true) {
+  public getUint16(littleEndian = true) {
     const result = this.dataView.getUint16(this.index, littleEndian)
     this.index += 2
     return result
   }
 
-  public getInt32 (littleEndian = true) {
+  public getInt32(littleEndian = true) {
     const result = this.dataView.getInt32(this.index, littleEndian)
     this.index += 4
     return result
   }
-  public getUint32 (littleEndian = true) {
+  public getUint32(littleEndian = true) {
     const result = this.dataView.getUint32(this.index, littleEndian)
     this.index += 4
     return result
   }
-  public getFloat32 (littleEndian = true) {
+  public getFloat32(littleEndian = true) {
     const result = this.dataView.getFloat32(this.index, littleEndian)
     this.index += 4
     return result
   }
-  public getFloat64 (littleEndian = true) {
+  public getFloat64(littleEndian = true) {
     const result = this.dataView.getFloat64(this.index, littleEndian)
     this.index += 8
     return result
   }
 
-  public getString (length?: number) {
+  public getString(length?: number) {
     const binary = this.getBinary(length)
     const array = [].slice.call(binary)
     return String.fromCharCode(...array)
   }
 
-  public getBinary (length?: number) {
+  public getBinary(length?: number) {
     if (length === undefined) {
       const result = new Uint8Array(this.arrayBuffer, this.index)
       this.index = this.arrayBuffer.byteLength
@@ -73,13 +73,13 @@ export class BinaryDecoder {
  * @public
  */
 export const BinaryEncoder = {
-  fromInt8 (...values: number[]) {
+  fromInt8(...values: number[]) {
     return new Uint8Array(new Int8Array(values).buffer)
   },
-  fromUint8 (...values: number[]) {
+  fromUint8(...values: number[]) {
     return new Uint8Array(values)
   },
-  fromInt16 (littleEndian: boolean, ...values: number[]) {
+  fromInt16(littleEndian: boolean, ...values: number[]) {
     const offset = 2
     const dataView = new DataView(new Uint8Array(values.length * offset).buffer)
     for (let i = 0; i < values.length; i++) {
@@ -87,7 +87,7 @@ export const BinaryEncoder = {
     }
     return new Uint8Array(dataView.buffer)
   },
-  fromUint16 (littleEndian: boolean, ...values: number[]) {
+  fromUint16(littleEndian: boolean, ...values: number[]) {
     const offset = 2
     const dataView = new DataView(new Uint8Array(values.length * offset).buffer)
     for (let i = 0; i < values.length; i++) {
@@ -95,7 +95,7 @@ export const BinaryEncoder = {
     }
     return new Uint8Array(dataView.buffer)
   },
-  fromInt32 (littleEndian: boolean, ...values: number[]) {
+  fromInt32(littleEndian: boolean, ...values: number[]) {
     const offset = 4
     const dataView = new DataView(new Uint8Array(values.length * offset).buffer)
     for (let i = 0; i < values.length; i++) {
@@ -103,7 +103,7 @@ export const BinaryEncoder = {
     }
     return new Uint8Array(dataView.buffer)
   },
-  fromUint32 (littleEndian: boolean, ...values: number[]) {
+  fromUint32(littleEndian: boolean, ...values: number[]) {
     const offset = 4
     const dataView = new DataView(new Uint8Array(values.length * offset).buffer)
     for (let i = 0; i < values.length; i++) {
@@ -111,7 +111,7 @@ export const BinaryEncoder = {
     }
     return new Uint8Array(dataView.buffer)
   },
-  fromFloat32 (littleEndian: boolean, ...values: number[]) {
+  fromFloat32(littleEndian: boolean, ...values: number[]) {
     const offset = 4
     const dataView = new DataView(new Uint8Array(values.length * offset).buffer)
     for (let i = 0; i < values.length; i++) {
@@ -119,7 +119,7 @@ export const BinaryEncoder = {
     }
     return new Uint8Array(dataView.buffer)
   },
-  fromFloat64 (littleEndian: boolean, ...values: number[]) {
+  fromFloat64(littleEndian: boolean, ...values: number[]) {
     const offset = 8
     const dataView = new DataView(new Uint8Array(values.length * offset).buffer)
     for (let i = 0; i < values.length; i++) {
@@ -127,14 +127,14 @@ export const BinaryEncoder = {
     }
     return new Uint8Array(dataView.buffer)
   },
-  fromString (unicode: string) {
+  fromString(unicode: string) {
     const uint8Array = new Uint8Array(unicode.length)
     for (let i = 0; i < unicode.length; i++) {
       uint8Array[i] = unicode.charCodeAt(i)
     }
     return uint8Array
   },
-  concat (...uint8Arrays: Uint8Array[]) {
+  concat(...uint8Arrays: Uint8Array[]) {
     const length = uint8Arrays.reduce((p, c) => p + c.length, 0)
     const result = new Uint8Array(length)
     let index = 0
