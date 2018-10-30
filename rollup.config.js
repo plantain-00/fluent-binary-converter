925abc53a0b072ec98e147a35dd57209e3c6f473
york yao <yaoao12306@outlook.com>
2018-10-30 22:58:21 +0800

    chore: add type-coverage check

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'dist/browser/index.js',
   name: 'FluentBinaryConverter',
-  plugins: [resolve(), uglify()],
+  plugins: [resolve({ browser: true }), uglify()],
   output: {
     file: 'dist/fluent-binary-converter.min.js',
     format: 'umd'
