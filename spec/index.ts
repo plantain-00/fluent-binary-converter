00fde620944e54ba58f97845de5fe06bd4beec0f
York Yao <me@yorkyao.com>
2020-06-14 06:25:40 +0800

    chore: clean code

--- spec/index.ts
+++ spec/index.ts
@@ -1,27 +1,28 @@
-import { BinaryDecoder, BinaryEncoder } from '../dist/nodejs'
+import test from 'ava'
+import { BinaryDecoder, BinaryEncoder } from '../src'
 
-it('', () => {
+test('BinaryDecoder BinaryEncoder', (t) => {
   const uint8Array1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 119, 119, 119])
   const binaryDecoder = new BinaryDecoder(uint8Array1.buffer as ArrayBuffer)
 
   const a = binaryDecoder.getUint32()
-  expect(a).toEqual(67305985)
+  t.is(a, 67305985)
 
   const b = binaryDecoder.getUint32()
-  expect(b).toEqual(134678021)
+  t.is(b, 134678021)
 
   const c = binaryDecoder.getString(3)
-  expect(c).toEqual('www')
+  t.is(c, 'www')
 
   const aBinary = BinaryEncoder.fromUint32(true, a)
-  expect(aBinary).toEqual(new Uint8Array([1, 2, 3, 4]))
+  t.deepEqual(aBinary, new Uint8Array([1, 2, 3, 4]))
 
   const bBinary = BinaryEncoder.fromUint32(true, b)
-  expect(bBinary).toEqual(new Uint8Array([5, 6, 7, 8]))
+  t.deepEqual(bBinary, new Uint8Array([5, 6, 7, 8]))
 
   const cBinary = BinaryEncoder.fromString('www')
-  expect(cBinary).toEqual(new Uint8Array([119, 119, 119]))
+  t.deepEqual(cBinary, new Uint8Array([119, 119, 119]))
 
   const uint8Array2 = BinaryEncoder.concat(aBinary, bBinary, cBinary)
-  expect(uint8Array2).toEqual(new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 119, 119, 119]))
+  t.deepEqual(uint8Array2, new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 119, 119, 119]))
 })
